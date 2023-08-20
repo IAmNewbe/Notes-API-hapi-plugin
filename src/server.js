8a6961954d30e59517c5d04dc35ddc49cc71d03c
IAmNewbe <ahlulhikam2222@gmail.com>
2023-08-21 06:07:13 +0700

    feat: export notes implemented

--- src/server.js
+++ src/server.js
@@ -26,6 +26,10 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+//Exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
 
 const init = async () => {
   const collaborationsService = new CollaborationsService();
@@ -96,6 +100,13 @@ const init = async () => {
         collaborationsService,
         notesService,
         validator: CollaborationsValidator,
+      }
+    },
+    {
+      plugin: _exports,
+      options: {
+        service: ProducerService,
+        validator: ExportsValidator,
       }
     }
   ]);
---
+++ src/validator/exports/index.js
@@ -0,0 +1,14 @@
+const InvariantError = require("../../exceptions/InvariantError");
+const ExportNotesPayloadSchema = require("./schema")
+
+const ExportsValidator = {
+  validateExportNotesPayload: (payload) => {
+    const validationResult = ExportNotesPayloadSchema.validate(payload);
+
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = ExportsValidator;
