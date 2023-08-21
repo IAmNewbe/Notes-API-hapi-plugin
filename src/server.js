d9f620d80c7c9aca380b4a55cc828036f5af1e73
IAmNewbe <ahlulhikam2222@gmail.com>
2023-08-21 11:36:08 +0700

    feat: add upload file

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const path = require('path');
 
 //notes
 const notes = require('./api/notes');
@@ -31,11 +32,18 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerService');
 const ExportsValidator = require('./validator/exports');
 
+//Uploads
+const Uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const uploads = require('./api/uploads');
+const UploadsValidator = require('./validator/uploads');
+
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(path.resolve(__dirname, 'api/uploads/file/images'));
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -107,6 +115,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      }
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       }
     }
   ]);
